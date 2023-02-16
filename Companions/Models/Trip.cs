1fd0c5857fd38b8ccc2c8e8ce9524fab362280d2
kana2022 <kkanat94@gmail.com>
2023-02-16 17:25:05 +0600

    added new models

---
+++ Companions/Models/Trip.cs
@@ -0,0 +1,10 @@
+﻿namespace Companions.Models;
+
+internal class Trip
+{
+    public int TripId { get; set; }
+    public string TripName { get; set;} 
+    public string TripDescription { get; set;}
+    List<string> TripTags { get; set; } = new List<string>();
+    public short IsActive { get; set; }
+}
---
+++ Companions/Models/TripTag.cs
@@ -0,0 +1,8 @@
+﻿namespace Companions.Models;
+
+internal class TripTag
+{
+    public int TagId { get; set; }
+    public string Tag { get; set; }
+    public int TripId { get; set; }
+}
