55c93e7b654a4b069ae0c5cda76537bc91b92bb5
kana2022 <kkanat94@gmail.com>
2023-02-16 11:59:56 +0600

    Add project files.

---
+++ Companions/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Companions;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Companions/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Companions;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
